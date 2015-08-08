fbe7da1aa22ad79e3d3822096bc43eca60545a43
doup <asier@illarra.com>
2015-08-08 12:23:02 +0200

    Emoji autocomplete hint

--- app/src/renderer.js
+++ app/src/renderer.js
@@ -33,6 +33,29 @@ $(function () {
     editor.setOption('scrollPastEnd', 1.0);
     editor.setOption('animatedScroll', true);
 
+    /// AUTOCOMPLETE
+
+    var langTools = ace.require("ace/ext/language_tools");
+    var emojis = require('../node_modules/markdown-it-emoji/lib/data/full.json');
+
+    var emojiCompleter = {
+        getCompletions: function (editor, session, pos, prefix, callback) {
+            callback(null, _.map(emojis, function (value, key) {
+                return { name: value, value: `:${key}:`, meta: 'emoji' };
+            }));
+
+            return;
+        }
+    };
+
+    langTools.setCompleters([emojiCompleter]);
+
+    editor.setOptions({
+        enableBasicAutocompletion: true,
+        enableSnippets: false,
+        enableLiveAutocompletion: false,
+    });
+
     /// SYNC SCROLL ///
 
     var mdHeadings, htmlHeadings;
